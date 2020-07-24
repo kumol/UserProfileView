c02e98771f0c598e84639f88fcea2c612b8bf9df
kumol <kumol0163@gmail.com>
2020-07-25 03:35:30 +0600

    state added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Components/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Person></Person>
-      </header>
-
-      
-    </div>
-  );
+class App extends Component {
+  state={
+    person:[
+      {name:"Anik",age:33},
+      {name:"Shamik",age:25}
+    ],
+    stage:true
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Person name={this.state.person[0].name}></Person>
+          <Person name={this.state.person[1].name}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Person =()=>{
+const Person =(props)=>{
     return(
         <div>
-            hello me from a custom component
+            hello me {props.name} from a custom component
         </div>
     )
 }

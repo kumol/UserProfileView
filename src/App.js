27cf59312e462826b0db427f91177fdc689a2dfd
kumol <kumol0163@gmail.com>
2020-07-25 04:26:09 +0600

    two way binding impleminted

--- src/App.js
+++ src/App.js
@@ -9,15 +9,31 @@ class App extends Component {
       {name:"Anik",age:33},
       {name:"Shamik",age:25}
     ],
-    stage:true
+    stage:false
+  }
+  nameChanger =(event)=>{
+    let newName=event.target.value;
+    this.setState({person:[
+      {name:newName},
+      {name:"max"}
+    ]})
+  }
+  changeName(){
+    this.setState({person:[{name:"kumol",age:25},{name:"Atik",age:25}]});
+  }
+  changeTheState(){
+    
+    console.log(this.state.stage)
+    //this.setState({stage:tate});
   }
   render(){
     return (
       <div className="App">
         <header className="App-header">
+          <button onClick={()=>this.changeName()}>Name Changer</button>
           <img src={logo} className="App-logo" alt="logo" />
-          <Person name={this.state.person[0].name}></Person>
-          <Person name={this.state.person[1].name}/>
+          <Person name={this.state.person[0].name} nameChanger={this.nameChanger} changes={this.changeName} state={this.state.stage} stateChanger={()=>this.changeTheState()}></Person>
+          <Person name={this.state.person[0].name} state={this.state.stage} stateChanger={()=>this.changeTheState()}/>
         </header>
       </div>
     );
--- src/Components/Person.js
+++ src/Components/Person.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Person =(props)=>{
     return(
         <div>
-            hello me {props.name} from a custom component
+            <p>hello me {props.name} from a custom component</p>
+            <input type="text" onChange={props.nameChanger} value={props.name}></input>
+            {/* {
+                props.state?<button onClick={props.stateChanger}>change state </button>:<button onClick={props.changes}> Name Change</button>
+            } */}
         </div>
     )
 }

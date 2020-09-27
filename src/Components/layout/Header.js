67906e1b4fee24ee8e34d06c21198e61f94a1b69
kumol <kumol01763@gmail.com>
2020-09-27 19:20:18 +0600

    router

---
+++ src/Components/layout/Header.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+const Header= ()=>{
+    return (
+      <div >
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+         <div className="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">React</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+               <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item active">
+                     <a class="nav-link" href="#">Home </a>
+                  </li>
+                  <li class="nav-item"> 
+                     <a class="nav-link" href="#">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">About</a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </nav>
+   </div>
+    )
+}
+export default Header;
--- src/Container/App.js
+++ src/Container/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import Home from '../Components/pages/Home';
+import Contact from '../Components/pages/Contact';
+import About from '../Components/pages/About';
+import Header from '../Components/layout/Header';
+import Sidebar from '../Components/layout/Sidebar';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+
 const App = ()=>{
   return(
-    <div>
-      <p>hello world</p>
+    <div >
+      <Router>
+      <Header></Header>
+      <div className="container"> 
+      <Home></Home>
+      <Contact></Contact>
+      <About></About>
+      </div>
+      </Router>
     </div>
   )
 }
